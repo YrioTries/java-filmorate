d5f5b60ebec06bac4dd0859cde099319177fa986
Balagur Dmitry <farnitin@yandex.ru>
2025-02-25 04:28:40 +0400

    configure project

--- src/main/java/ru/yandex/practicum/filmorate/handler/ErrorHandler.java
+++ src/main/java/ru/yandex/practicum/filmorate/handler/ErrorHandler.java
@@ -1,12 +1,17 @@
 package ru.yandex.practicum.filmorate.handler;
 
+import jakarta.validation.ConstraintViolationException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import ru.yandex.practicum.filmorate.exception.NotFoundException;
 import ru.yandex.practicum.filmorate.exception.ValidationException;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Slf4j
 @RestControllerAdvice
@@ -15,8 +20,28 @@ public class ErrorHandler {
     @ExceptionHandler(ValidationException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ErrorResponse validationError(final ValidationException e) {
-        log.info("ERROR[400]: Произошла ошибка ValidationException: {}", e.getMessage());
+        log.warn("ERROR[400]: Произошла ошибка ValidationException: {}", e.getMessage());
         return new ErrorResponse(e.getMessage());
+    }
+
+    @ExceptionHandler(ConstraintViolationException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorResponse constraintViolationError(final ConstraintViolationException e) {
+        log.warn("ERROR[400]: Произошла ошибка ConstraintViolationException: {}", e.getMessage());
+        return new ErrorResponse(e.getMessage());
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorResponse handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
+        log.error("ERROR[400]: MethodArgumentNotValidException: {}", e.getMessage());
+
+        List<String> errors = e.getBindingResult()
+                .getFieldErrors()
+                .stream()
+                .map(fieldError -> fieldError.getDefaultMessage())
+                .collect(Collectors.toList());
+        return new ErrorResponse(String.join(", ", errors));
     }
 
     @ExceptionHandler(NotFoundException.class)
@@ -29,7 +54,7 @@ public class ErrorHandler {
     @ExceptionHandler(Exception.class)
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     public ErrorResponse exceptionError(final Exception e) {
-        log.info("ERROR[500]: Произошла ошибка ErrorException: {}", e.getMessage());
+        log.error("ERROR[500]: Произошла ошибка Exception: {}", e.getMessage(), e);
         return new ErrorResponse(e.getMessage());
     }
 }
