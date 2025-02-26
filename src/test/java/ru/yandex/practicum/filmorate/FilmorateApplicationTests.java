01d667eadf91dce8eb62238d8a668fe4c157841b
Balagur Dmitry <farnitin@yandex.ru>
2025-02-26 23:17:06 +0400

    fix style

--- src/test/java/ru/yandex/practicum/filmorate/FilmorateApplicationTests.java
+++ src/test/java/ru/yandex/practicum/filmorate/FilmorateApplicationTests.java
@@ -1,7 +1,6 @@
 package ru.yandex.practicum.filmorate;
 
 import jakarta.validation.ConstraintViolationException;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
