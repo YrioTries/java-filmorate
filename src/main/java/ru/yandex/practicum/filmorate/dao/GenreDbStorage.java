61dbef3de6c942f1c2b69e084cff3beea83e6212
Balagur Dmitry <farnitin@yandex.ru>
2025-05-06 07:55:14 +0400

    fix genre logic

--- src/main/java/ru/yandex/practicum/filmorate/dao/GenreDbStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/dao/GenreDbStorage.java
@@ -2,35 +2,62 @@ package ru.yandex.practicum.filmorate.dao;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.context.annotation.Primary;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.jdbc.core.JdbcOperations;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
+import ru.yandex.practicum.filmorate.exception.NotFoundException;
 import ru.yandex.practicum.filmorate.model.Genre;
 import ru.yandex.practicum.filmorate.storage.GenreStorage;
 
-import java.util.Collection;
-import java.util.Optional;
+import java.util.List;
 
 @Slf4j
 @Repository
+@Primary
 public class GenreDbStorage implements GenreStorage {
-    private final JdbcTemplate jdbcTemplate;
+    private final JdbcOperations jdbc;
+    private final RowMapper<Genre> mapper;
 
     @Autowired
-    public GenreDbStorage(JdbcTemplate jdbcTemplate) {
-        this.jdbcTemplate = jdbcTemplate;
+    public GenreDbStorage(JdbcOperations jdbc, RowMapper<Genre> mapper) {
+        this.jdbc = jdbc;
+        this.mapper = mapper;
     }
 
     @Override
-    public Collection<Genre> findAll() {
-        log.info("Получение всех жанров из базы данных");
-        String sql = "SELECT * FROM genres ORDER BY id";
-        return jdbcTemplate.query(sql, (rs, rowNum) -> new Genre(rs.getLong("id"), rs.getString("name")));
+    public List<Genre> getAllGenres() {
+        final String FIND_ALL_QUERY = """
+                SELECT *
+                FROM genres;
+                """;
+
+        List<Genre> genres = jdbc.query(FIND_ALL_QUERY, mapper);
+        if (genres == null || genres.isEmpty()) {
+            return List.of();
+        }
+        return genres;
     }
 
     @Override
-    public Optional<Genre> getGenre(Long id) {
-        log.info("Получение жанра с id: {} из базы данных", id);
-        String sql = "SELECT * FROM genres WHERE id = ?";
-        return jdbcTemplate.query(sql, rs -> rs.next() ? Optional.of(new Genre(rs.getLong("id"), rs.getString("name"))) : Optional.empty(), id);
+    public Genre getGenreById(Integer id) {
+        final String FIND_BY_ID_QUERY = """
+                SELECT *
+                FROM genres
+                WHERE genre_id = ?;
+                """;
+
+        Genre genre;
+        try {
+            genre = jdbc.queryForObject(FIND_BY_ID_QUERY, mapper, id);
+        } catch (EmptyResultDataAccessException ignored) {
+            genre = null;
+        }
+        if (genre == null) {
+            log.warn("GenreDbStorage: Не удалось получить объект Genre по его ID - не найден в приложении");
+            throw new NotFoundException("GenreDbStorage: Жанр c ID: " + id + " не найден в приложении");
+        }
+        return genre;
     }
 }
